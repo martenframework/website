09bf3671114a133968fff6b2adbc8ee224d8149e
Morgan Aubert <morgan@aubert.email>
2022-01-03 17:15:01 -0500

    Ensure allowed_hosts setting is properly set in production

--- config/settings/production.cr
+++ config/settings/production.cr
@@ -1,6 +1,7 @@
 Marten.configure :production do |config|
   config.debug = false
   config.host = "0.0.0.0"
+  config.allowed_hosts = EnvSetting.fetch(:ALLOWED_HOSTS).split(",")
 
   config.basic_auth.username = EnvSetting.fetch(:BASIC_AUTH_USERNAME, "insecure")
   config.basic_auth.password = EnvSetting.fetch(:BASIC_AUTH_PASSWORD, "insecure")
