bc4873215b7aedd46a254805cea4700b0fbdd4b1
Morgan Aubert <morgan@aubert.email>
2022-04-04 18:26:08 -0400

    Add custom 404 and 500 pages

--- config/routes.cr
+++ config/routes.cr
@@ -3,5 +3,7 @@ Marten.routes.draw do
 
   if Marten.env.development?
     path "#{Marten.settings.assets.url}<path:path>", Marten::Views::Defaults::Development::ServeAsset, name: "asset"
+    path "/404", Marten.settings.view404, name: "404"
+    path "/500", Marten.settings.view500, name: "500"
   end
 end
