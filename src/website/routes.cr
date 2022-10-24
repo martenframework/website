06d1529dc36a5e0bc5be9dac432f4fcef83350b6
Morgan Aubert <morgan@aubert.email>
2022-10-23 20:12:19 -0400

    Disable bad route

--- src/website/routes.cr
+++ src/website/routes.cr
@@ -1,6 +1,6 @@
 module Website
   ROUTES = Marten::Routing::Map.draw do
     path "/", HomeHandler, name: "home"
-    path "/bad", BadHandler, name: "bad"
+    # path "/bad", BadHandler, name: "bad"
   end
 end
