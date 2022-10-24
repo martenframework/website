a46c01dfaebd8988e58b8eb54ba4530a6dd9e63d
Morgan Aubert <morgan@aubert.email>
2022-10-24 17:21:43 -0400

    Fix eslint warnings

--- src/website/assets/js/controllers/home.js
+++ src/website/assets/js/controllers/home.js
@@ -1,17 +1,15 @@
-
 import hljs from 'highlight.js';
-
 
 export default {
   init() {
     // Initializes Highlight JS.
     document.querySelectorAll('pre code.language-html').forEach((el) => {
       el.innerHTML = el.innerHTML
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;");
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
     });
 
     document.querySelectorAll('pre code').forEach((el) => {
