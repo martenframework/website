435ec5b92e3f68b55c314af7ecf20f28031de726
Morgan Aubert <morgan@aubert.email>
2022-10-23 20:12:46 -0400

    Disable basic auth

--- config/settings/production.cr
+++ config/settings/production.cr
@@ -4,14 +4,10 @@ Marten.configure :production do |config|
   config.allowed_hosts = EnvSetting.fetch(:ALLOWED_HOSTS, "").split(",")
   config.use_x_forwarded_proto = true
 
-  config.basic_auth.username = EnvSetting.fetch(:BASIC_AUTH_USERNAME, "insecure")
-  config.basic_auth.password = EnvSetting.fetch(:BASIC_AUTH_PASSWORD, "insecure")
-
   config.strict_transport_security.max_age = 3600
 
   config.middleware.unshift(
     RavenMiddleware,
-    BasicAuthMiddleware,
     WWWRedirectMiddleware,
   )
 
